df29e80ab1fcd4d215f9004751d943b82dfd40e2
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-03-17 21:34:25 +0600

    diff between c & cpp

---
+++ day-7.c
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Student
+{
+    public:
+        int id_no;
+        char name[21];
+
+};
+
+int main(void)
+{
+
+    raturn 0
+}
