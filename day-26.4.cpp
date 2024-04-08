98d663bc9bf8be2c86d20a249348fdca466e5008
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-04-08 15:12:02 +0600

    private member inheritance public mode

---
+++ day-26.4.cpp
@@ -0,0 +1,56 @@
+// private member inheritance public mode
+// there is no way private member inheritance in any mode like public, private, protected
+#include <iostream>
+
+using namespace std;
+
+// base class
+class Student
+{
+    private:
+        int roll;
+        string name;
+
+    public:
+        // by pass way, we access private member in another class
+        void set(){
+            cout << "Enter Your Roll & Name: ";
+            cin >> roll >> name;
+        }
+        void print(){
+            cout << "Roll & Name are: " << roll << " " << name << endl;
+        }
+};
+
+// derived class
+class Result: public Student
+{
+    public:
+        void all(){
+            set();
+            print();
+        }
+};
+
+/*
+    protected mode
+    class Result{
+        protected:
+            int roll;
+            string name;
+        public:
+            void set();
+            void print();
+    }
+*/
+
+int main(void)
+{
+    Result s1;
+
+    // s1.set();
+    // s1.print();
+    s1.all();
+
+    return 0;
+}
