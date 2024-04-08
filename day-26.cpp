932aab22c372eb5978f930ae1821fb7f6ce453d2
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-04-08 14:42:53 +0600

    public mode inheritance

---
+++ day-26.cpp
@@ -0,0 +1,41 @@
+// public mode inheritance
+#include <iostream>
+
+using namespace std;
+
+// base class
+class Student
+{
+    public:
+        int roll;
+        string name;
+};
+
+// derived class
+class Result: public Student
+{
+    public:
+        void set(){
+            cout << "Enter Your Roll & Name: ";
+            cin >> roll >> name;
+        }
+        void print(){
+            cout << "Roll & Name are: " << roll << " " << name << endl;
+        }
+};
+
+int main(void)
+{
+    Result s1;
+
+    s1.roll = 101;
+    s1.name = "Muhammad Al-Amin";
+
+    // cout << "Roll & Name are: " << s1.roll << " " << s1.name << endl;
+    s1.set();
+    s1.print();
+
+    return 0;
+}
+
+
