9deea9b924e17db33de4ce6cff5131ba9351ae36
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-04-08 14:56:43 +0600

    public member inheritance protected mode

---
+++ day-26.3.cpp
@@ -0,0 +1,55 @@
+// public member inheritance protected mode
+#include <iostream>
+
+using namespace std;
+
+// base class
+class Student
+{
+    public:
+        int roll;
+        string name;
+};
+
+// derived class
+class Result: protected Student
+{
+    public:
+        void set(){
+            cout << "Enter Your Roll & Name: ";
+            cin >> roll >> name;
+        }
+        void print(){
+            cout << "Roll & Name are: " << roll << " " << name << endl;
+        }
+};
+
+/*
+    protected mode
+    class Result{
+        protected:
+            int roll;
+            string name;
+        public:
+            void set();
+            void print();
+    }
+*/
+
+int main(void)
+{
+    Result s1;
+
+    // s1.roll = 101; // error
+    // s1.name = "Muhammad Al-Amin"; // error
+
+    // cout << "Roll & Name are: " << s1.roll << " " << s1.name << endl;
+    s1.set();
+    s1.print();
+
+    return 0;
+}
+
+
+
+
