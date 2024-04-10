67bf386a373aec83ffabe0ce043b70ddcb99eeb0
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-04-10 08:39:09 +0600

    multilevel inheritance

---
+++ day-28.cpp
@@ -0,0 +1,57 @@
+// multilevel inheritance
+#include <iostream>
+
+using namespace std;
+
+class Info
+{
+    private:
+        int id;
+    public:
+        void set_id();
+        int get_id();
+};
+
+class Result : public Info
+{
+    private:
+        int mark;
+    public:
+        void get_mark();
+};
+
+class Student: public Result
+{
+    public:
+        void print();
+};
+
+void Info::set_id(){
+    cout << "Enter ID: ";
+    cin >> id;
+}
+
+int Info::get_id(){
+    return id;
+}
+
+void Result::get_mark(){
+    cout << "Enter Mark: ";
+    cin >> mark;
+    cout << "Marks Is: " << mark << endl;
+}
+
+void Student::print(){
+    cout << "ID is: " << get_id() << endl;
+    get_mark();
+}
+
+int main(void)
+{
+    Student s1;
+
+    s1.set_id();
+    s1.print();
+
+    return 0;
+}
